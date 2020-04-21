1344013f571e6a59eca49248940df58b6330ca19
LauroFreitas <57110651+LauroFreitas@users.noreply.github.com>
2020-04-21 00:07:27 -0300

    Add files via upload

---
+++ Buttons.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Buttons : MonoBehaviour
+{
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
