fc5ce2685b91d5df0c1599154325bfd1e24fe54d
Raf-El <63626443+Raf-El@users.noreply.github.com>
2020-04-20 23:54:03 -0300

    Add files via upload

---
+++ GetInput.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class GetInput : MonoBehaviour
+{
+
+    public InputField valorGasolina;
+    public InputField valorEtanol;
+    private float gasolina;
+    private float etanol;
+    public Text testo;
+    public Text etanolText;
+    public Text gasolinaText;
+
+    private void Start()
+    {
+        etanolText.enabled = false;
+        gasolinaText.enabled = false;
+    }
+    public void Setget() 
+    {
+        if (valorEtanol == null) 
+        {
+            testo.text = "É nescessario preencher todos os componentes";
+            return;
+        }
+        else if (valorGasolina == null)
+        {
+            testo.text = "É nescessario preencher todos os componentes";
+            return;
+        }
+        else
+        {
+            gasolina = float.Parse(valorGasolina.text);
+            etanol = float.Parse(valorEtanol.text);
+            float valor3 = etanol / gasolina;
+
+            if (valor3 < 0.7)
+            {
+                etanolText.enabled = true;
+                testo.text = "O melhor rendimento é de etanol ";
+                gasolinaText.enabled = false;
+            }
+            else
+            {
+                gasolinaText.enabled = true;
+                testo.text = "O melhor rendimento é de gasolina ";
+                etanolText.enabled = false;
+            }
+        }
+    }
+ }
